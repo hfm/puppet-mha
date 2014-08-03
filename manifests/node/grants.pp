0dee2bed96298a23e9262cd2bc7107a697115440
lamanotrama <lamanotrama@gmail.com>
2014-08-04 03:15:40 +0900

    mysql_grantの仕様上無理… Revert "面倒なので、grantは個別ホストでなくprovate network全体に付与する"
    This reverts commit b51bdf578c27fa42108306ebca690ca5756734ef.
    
    host名に/が入ると、移行をdb名として切り出す謎仕様だった。

--- manifests/node/grants.pp
+++ manifests/node/grants.pp
@@ -1,19 +1,36 @@
 class mha::node::grants {
 
-  mysql::rights { 'all grants on root@internal':
-    user     => 'root',
-    host     => "${::network_eth1}/${::netmask_eth1}",
-    password => 'percona';
+  mysql::rights {
+    'all grants manager001':
+      user     => 'root',
+      host     => 'manager001.mha.lan',
+      password => 'percona';
+
+    'all grants node001':
+      user     => 'root',
+      host     => 'node001.mha.lan',
+      password => 'percona';
+
+    'all grants node002':
+      user     => 'root',
+      host     => 'node002.mha.lan',
+      password => 'percona';
+
+    'all grants node003':
+      user     => 'root',
+      host     => 'node003.mha.lan',
+      password => 'percona';
   }
 
-  mysql::rights { 'repl grants':
-    user     => 'repl',
-    host     => "${::network_eth1}/${::netmask_eth1}",
-    password => 'replication',
-    priv     => [
-      'repl_client_priv',
-      'repl_slave_priv',
-    ];
+  mysql::rights {
+    'repl grants':
+      user     => 'repl',
+      host     => '%',
+      password => 'replication',
+      priv     => [
+        'repl_client_priv',
+        'repl_slave_priv',
+      ];
   }
 
 }
