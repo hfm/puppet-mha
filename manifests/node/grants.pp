477efeed35afdf7798e36b5e3e060ed258cbeee9
lamanotrama <lamanotrama@gmail.com>
2014-08-09 22:16:44 +0900

    mysqlモジュールはlibrarian-puppetでinstallして、それを使う

--- manifests/node/grants.pp
+++ manifests/node/grants.pp
@@ -3,6 +3,7 @@ class mha::node::grants {
     $mha::node::manager:
       user     => $mha::node::user,
       password => $mha::node::password;
+
     $mha::node::nodes:
       user     => $mha::node::user,
       password => $mha::node::password;
@@ -12,6 +13,7 @@ class mha::node::grants {
     $mha::node::manager:
       user     => $mha::node::repl_user,
       password => $mha::node::repl_password;
+
     $mha::node::nodes:
       user     => $mha::node::repl_user,
       password => $mha::node::repl_password;
@@ -23,10 +25,14 @@ define mha::node::grants::admin (
   $user,
   $password,
 ) {
-  mysql::rights { "all grants ${user}@${host}":
-    user     => $user,
-    host     => $host,
-    password => $password,
+  mysql_user { "${user}@${host}":
+    password_hash => mysql_password($password),
+  }
+
+  mysql_grant { "${user}@${host}/*.*":
+    user       => "${user}@${host}",
+    table      => '*.*',
+    privileges => ['ALL'],
   }
 }
 
@@ -35,13 +41,16 @@ define mha::node::grants::repl (
   $user,
   $password,
 ) {
-  mysql::rights { "repl grants ${user}@${host}":
-    user     => $user,
-    host     => $host,
-    password => $password,
-    priv     => [
-      'repl_client_priv',
-      'repl_slave_priv',
-    ];
+  mysql_user { "${user}@${host}":
+    password_hash => mysql_password($password),
+  }
+
+  mysql_grant { "${user}@${host}/*.*":
+    user       => "${user}@${host}",
+    table      => '*.*',
+    privileges => [
+      'REPLICATION SLAVE',
+      'REPLICATION CLIENT',
+    ],
   }
 }
