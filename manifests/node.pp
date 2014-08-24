5dee4abf38276dc82d8c4b5e1949f36c3da2fd13
lamanotrama <lamanotrama@gmail.com>
2014-08-25 01:07:53 +0900

    mysql-serverのinstallとサービス管理をmysql::serverモジュールに移譲

--- manifests/node.pp
+++ manifests/node.pp
@@ -16,7 +16,15 @@ class mha::node (
   class { 'mha::node::install': version => $version }
   -> class { 'mha::node::grants': }
 
-  Service['mysql']
+  # if puppetlabs-mysql is used, and mysql::server class is included with service_manage parameter(default true)
+  # ref https://github.com/puppetlabs/puppetlabs-mysql/blob/master/manifests/server/service.pp#L10
+  if $mysql::server::real_service_manage {
+    $service_name = $mysql::server::service_name
+  } else {
+    $service_name = 'mysql'
+  }
+
+  Service[$service_name]
   -> Class['mha::node::grants']
 
   create_resources(mha::ssh_keys, { "mha::node" => $ssh } )
