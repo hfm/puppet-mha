f429e1ce3e242349105328b8c098bc0e62dd6231
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-03-24 12:11:20 +0900

    Use ensure params to configure mysql_online_switch file to manage

--- manifests/manager/script.pp
+++ manifests/manager/script.pp
@@ -1,6 +1,20 @@
-class mha::manager::script {
+# Class: mha::manager::script
+# ===========================
+#
+# Configure mysql_online_switch file to manage.
+#
+# Parameters
+# ----------
+#
+# * `ensure`
+# Whether the mysql_online_switch script should exist.
+#
+class mha::manager::script (
+  $ensure = present,
+) {
 
   file { '/usr/bin/mysql_online_switch':
+    ensure  => $ensure,
     content => file('mha/usr/bin/mysql_online_switch'),
     mode    => '0755',
   }
