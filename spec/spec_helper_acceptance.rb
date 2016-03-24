f2b4db9371fc0925932058967dca82a5ed8f7f5c
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-03-24 09:11:47 +0900

    Acceptance tests of centos6

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,21 @@
+require 'beaker-rspec'
+
+# Install Puppet agent on all hosts
+install_puppet_agent_on(hosts, {})
+
+RSpec.configure do |c|
+  c.formatter = :documentation
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  c.before :suite do
+    # Install module to all hosts
+    install_dev_puppet_module(:source => module_root)
+
+    # Install dependencies
+    hosts.each do |host|
+      on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
+      on(host, puppet('module', 'install', 'puppetlabs-mysql'))
+      on(host, puppet('module', 'install', 'proletaro-supervisor'))
+    end
+  end
+end
