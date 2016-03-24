385373b5316f14d89e8514a0ea741e160c72819c
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-03-24 11:31:40 +0900

    Use file function instead of source

--- manifests/manager/script.pp
+++ manifests/manager/script.pp
@@ -1,7 +1,7 @@
 class mha::manager::script {
 
   file { '/usr/bin/mysql_online_switch':
-    source => 'puppet:///modules/mha/usr/bin/mysql_online_switch',
+    content => file('mha/usr/bin/mysql_online_switch'),
     mode   => '0755',
   }
 
