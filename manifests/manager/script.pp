c7da101c977fa62e3ab5b3a8571330517da5c855
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-03-24 12:09:59 +0900

    Fix lint

--- manifests/manager/script.pp
+++ manifests/manager/script.pp
@@ -2,7 +2,7 @@ class mha::manager::script {
 
   file { '/usr/bin/mysql_online_switch':
     content => file('mha/usr/bin/mysql_online_switch'),
-    mode   => '0755',
+    mode    => '0755',
   }
 
 }
