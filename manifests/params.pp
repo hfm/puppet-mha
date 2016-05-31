dfa8d31e24ec0ded20912b2b284071d9ca9d0bff
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-05-31 14:00:19 +0900

    style

--- manifests/params.pp
+++ manifests/params.pp
@@ -25,8 +25,8 @@ class mha::params {
   $purge_relay_logs_minute = '10'
   $purge_relay_logs_hour   = '2-23/6' # 2,8,14,20
 
-  $ssh_user = 'root'
-  $ssh_port = '22'
+  $ssh_user        = 'root'
+  $ssh_port        = '22'
   $ssh_key_path    = '/root/.ssh/id_mha'
   $ssh_key_type    = 'rsa'
   $ssh_public_key  = 'AAAAB3NzaC1yc2EAAAABIwAAAQEArtdvtkbHIW0GOUcDSBJtK0ql14YMLGxyvROqWUzH89UhMzUoMyViFMYdKDJJ/99F/vTiMtTl4lAyw93UVTMAyI1wfDlUjv9CDEWshuFUW6FPRSYJq+zo9GXrLZK84UJItjngP5Zdxuyd38chSPfnBG5scrlsgwVZmQEU9wStWhYewU/DKvXmMvX7b2nzmvaaTKdl6tAOq5ZFC51DVaYk1dpNW8LpjL617Gfj69z5ot+zexXZgIIJapjmpyaqSeGg834W9YaUFtCGXnKIrOQuseiRz4TE0wLJ5V+4VqQM6CDrN90QX23WTXLLkkvMXYg97rbPriO8+T5y7t9ugYx/Qw=='
