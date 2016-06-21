6c0f8e21b3cab38f466e94a6fbb79d932d97d040
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-06-21 22:14:30 +0900

    Expect exit status 2 at first apply.

--- spec/acceptance/mha_node_spec.rb
+++ spec/acceptance/mha_node_spec.rb
@@ -31,9 +31,8 @@ describe 'mha::node class' do
   }
 
   it 'should work without errors' do
-    result = apply_manifest(manifest, :acceptable_exit_codes => [0, 2], :catch_failures => true)
-    expect(result.exit_code).not_to eq 4
-    expect(result.exit_code).not_to eq 6
+    result = apply_manifest(manifest, :catch_failures => true)
+    expect(result.exit_code).to eq 2
   end
 
   it 'should run a second time without changes' do
