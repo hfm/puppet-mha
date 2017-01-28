2eff47f1f040d9e0f9cfff20ec8d97822c55687e
takumakume <takuma.kume@gmail.com>
2017-01-28 15:32:11 +0900

    Use RPMs from this repository, because original RPM repository (Google Drive) can't be used

--- manifests/node/install.pp
+++ manifests/node/install.pp
@@ -2,26 +2,22 @@ class mha::node::install (
   $version = $mha::node::version,
 ) {
 
-  $ensure        = "${version}.el${::operatingsystemmajrelease}"
-  $rpm           = "mha4mysql-node-${ensure}.noarch.rpm"
-  $source_url    = "https://72003f4c60f5cc941cd1c7d448fc3c99e0aebaa8.googledrive.com/host/0B1lu97m8-haWeHdGWXp0YVVUSlk/${rpm}"
-  $download_path = "/usr/local/src/${rpm}"
+  $ensure   = "${version}.el${::operatingsystemmajrelease}"
+  $rpm      = "mha4mysql-node-${ensure}.noarch.rpm"
+  $rpm_path = "/usr/local/src/${rpm}"
 
   ensure_packages('perl-DBD-MySQL')
 
-  # Because the rpm command on centos5 is failed.
-  exec { 'download mha-node':
-    command => "curl -L -o ${download_path} '${source_url}'",
-    path    => ['/bin', '/usr/bin', '/usr/local/bin'],
-    creates => $download_path,
+  file { $rpm_path:
+    source => "puppet:///modules/mha/${rpm}",
   }
 
   package { 'mha4mysql-node':
     ensure   => $ensure,
     provider => rpm,
-    source   => $download_path,
+    source   => $rpm_path,
     require  => [
-      Exec['download mha-node'],
+      File[$rpm_path],
       Package['perl-DBD-MySQL'],
     ],
   }
